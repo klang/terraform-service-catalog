ddc66573d85e4165d15fdecdd44fdde0d732537c
Karsten Lang Pedersen <karstenlang@gmail.com>
2023-05-04 14:55:54 +0200

    place state in s3

--- backend.tf
+++ backend.tf
@@ -1,20 +1,19 @@
 terraform {
   required_version = "<=1.3.9"
   /*
-  awsume training
-  aws s3 mb s3://<accountid>-<project>-terraform-state
+  awsume shared-services
+  aws s3 mb s3://940740948575-service-catalog-terraform-state
   awsume iam
   terraform init
   # answer "yes" to change the backend
   */
-  /*
+
   backend "s3" {
-    bucket = "s3://<accountid>-<project>-terraform-state"
+    bucket = "940740948575-service-catalog-terraform-state"
     key = "terraform.tfstate"
     region = "eu-west-1"
     external_id = "terraform"
     session_name = "terraform"
-    role_arn = "arn:aws:iam::<accountid>:role/terraform"
+    role_arn = "arn:aws:iam::940740948575:role/terraform"
   }
-  */
 }
