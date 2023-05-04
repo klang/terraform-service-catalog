5b9ad06598dd3e0f63ad3f3acd2737f5dc8a9322
Karsten Lang Pedersen <karstenlang@gmail.com>
2023-05-04 09:02:04 +0200

    naming

---
+++ delegated_administrator.tf
@@ -0,0 +1,35 @@
+data "aws_organizations_delegated_administrators" "on_shared" {
+    provider = aws.shared
+    service_principal = "servicecatalog.amazonaws.com"
+}
+output "delegated_administrator_service_catalog" {
+    value = data.aws_organizations_delegated_administrators.on_shared
+}
+
+data "aws_organizations_delegated_administrators" "for_stacksets" {
+    provider = aws.shared
+    service_principal = "member.org.stacksets.cloudformation.amazonaws.com"
+    depends_on = [ aws_organizations_delegated_administrator.member_for_stacksets ]
+}
+
+output "delegated_administrator_stack_sets" {
+    value = data.aws_organizations_delegated_administrators.for_stacksets
+}
+
+resource "aws_organizations_delegated_administrator" "member_for_stacksets" {
+    provider = aws.master
+    account_id        = local.account_id
+    service_principal = "member.org.stacksets.cloudformation.amazonaws.com"
+}
+
+
+/* resource "aws_organizations_delegated_administrator" "management_for_stacksets" {
+    provider = aws.master
+    account_id        = local.account_id
+    service_principal = "stacksets.cloudformation.amazonaws.com"
+} */
+
+
+
+#  awsume controltower
+#  aws organizations list-delegated-services-for-account --account-id 940740948575
